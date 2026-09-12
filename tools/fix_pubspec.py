65415aab670362195ebafd61c8564727e3eb4ea4
closedwindy <closedwindy@users.noreply.github.com>
2026-09-12 02:52:17 +0000

    feat: add fix_pubspec.py helper

---
+++ tools/fix_pubspec.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Remove ohos dependencies from pubspec.yaml and fix video_player version."""
+import re
+import sys
+
+pubspec_path = sys.argv[1] if len(sys.argv) > 1 else "pubspec.yaml"
+text = open(pubspec_path).read()
+
+# Fix video_player version
+text = text.replace("video_player: ^2.11.1", "video_player: ^2.10.1")
+
+# Remove ohos dependency blocks (key + indented sub-keys)
+for name in ["video_player_ohos", "path_provider_ohos", "record_ohos", "url_launcher_ohos", "file_selector_ohos"]:
+    pattern = r"(?m)^  " + name + r":.*?\n(?:    .*\n)*"
+    text = re.sub(pattern, "", text)
+
+open(pubspec_path, "w").write(text)
+print("Done: ohos deps removed, video_player fixed")
