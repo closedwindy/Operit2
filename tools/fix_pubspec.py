8febb63b6bb99598d8cae1629fafa43f030ef7b5
closedwindy <closedwindy@users.noreply.github.com>
2026-09-12 05:33:18 +0000

    trigger: fix maven+venv

--- tools/fix_pubspec.py
+++ tools/fix_pubspec.py
@@ -17,3 +17,4 @@ for name in ["video_player_ohos", "path_provider_ohos", "record_ohos", "url_laun
 open(pubspec_path, "w").write(text)
 print("Done: ohos deps removed, video_player fixed")# trigger build
 # trigger
+# trigger
